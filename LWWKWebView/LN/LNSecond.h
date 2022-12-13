434b5b3c4078098340c05e4a39c576738ba9910e
a295400581 <295400581@qq.com>
2022-12-13 16:18:52 +0800

    1.0.5版本加入c++代码

---
+++ LWWKWebView/LN/LNSecond.h
@@ -0,0 +1,16 @@
+//
+//  LNSecond.h
+//  TestLWWKWebView2
+//
+//  Created by  LW on 2022/12/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LNSecond : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LWWKWebView/LN/LNSecond.m
@@ -0,0 +1,12 @@
+//
+//  LNSecond.m
+//  TestLWWKWebView2
+//
+//  Created by  LW on 2022/12/13.
+//
+
+#import "LNSecond.h"
+
+@implementation LNSecond
+
+@end
